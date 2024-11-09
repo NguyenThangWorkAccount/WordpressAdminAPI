8112816af16695b7bf0d82bfc102c41c30bc6d62
Nguyễn Đình Thắng <nguyenthang.workmail@gmail.com>
2024-11-09 12:37:09 +0700

    Add logic for managing server start/stop via command-line arguments

---
+++ Configs/GoogleSheetConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace WordpressAdminApi
+{
+    public class GoogleSheetsConfig
+    {
+        public string ApiKeyPath { get; }
+        public string SheetId { get; }
+
+        public GoogleSheetsConfig(string apiKeyPath, string sheetId)
+        {
+            if (string.IsNullOrWhiteSpace(apiKeyPath))
+                throw new ArgumentException("ApiKeyPath cannot be null or empty.", nameof(apiKeyPath));
+
+            if (string.IsNullOrWhiteSpace(sheetId))
+                throw new ArgumentException("SheetId cannot be null or empty.", nameof(sheetId));
+
+            ApiKeyPath = apiKeyPath;
+            SheetId = sheetId;
+        }
+    }
+}
---
+++ Utilities/SheetsServiceInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+
+namespace WordpressAdminAPI.Utilities
+{
+    public class SheetsServiceInitializer
+    {
+        private readonly string apiKeyPath;
+
+        public SheetsServiceInitializer(string apiKeyPath)
+        {
+            this.apiKeyPath = apiKeyPath;
+        }
+
+        public SheetsService InitializeSheetsService()
+        {
+            GoogleCredential credential;
+            using (var stream = new FileStream(apiKeyPath, FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.Spreadsheets);
+            }
+
+            return new SheetsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "WordpressAdminApi"
+            });
+        }
+    }
+}
